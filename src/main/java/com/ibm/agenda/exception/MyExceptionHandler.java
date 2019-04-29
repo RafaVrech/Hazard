e5295eefcbeb0fb33080126368e6bd3950581b7e
RafaelAntonioVrech <RafaelAntonioVrech@LAPTOP-FTE3CG62.br.ibm.com>
2019-04-29 11:10:57 -0300

    erro import

--- src/main/java/com/ibm/agenda/exception/MyExceptionHandler.java
+++ src/main/java/com/ibm/agenda/exception/MyExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.ibm.agenda.exception;
 
-import com.ibm.agenda.controller.Resposta;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +7,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.hazard.utils.Resposta;
 
 @ControllerAdvice
 public class MyExceptionHandler extends ResponseEntityExceptionHandler {

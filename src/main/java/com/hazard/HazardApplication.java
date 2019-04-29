7aceb28bf03a4f1d173c0032c681bbdade862b4f
RafaelAntonioVrech <RafaelAntonioVrech@LAPTOP-FTE3CG62.br.ibm.com>
2019-04-29 11:30:35 -0300

    Correcao pacote do application

--- src/main/java/com/hazard/HazardApplication.java
+++ src/main/java/com/hazard/HazardApplication.java
@@ -1,11 +1,13 @@
-package com.hazard.Hazard;
+package com.hazard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan( basePackages = {"com.hazard.model"} )
+@ComponentScan
 public class HazardApplication {
 
 	public static void main(String[] args) {
